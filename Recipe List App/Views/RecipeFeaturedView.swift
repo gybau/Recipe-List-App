d67b673a2128ca0f402c8fe0efc0eab2b551b717
gybau <115479476+gybau@users.noreply.github.com>
2022-10-11 23:09:47 +0200

    - create RecipeFeaturedView - add RecipeFeaturedView to RecipeTabView

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -11,7 +11,45 @@ struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Featured Recipes")
+                .font(.title)
+                .bold()
+            GeometryReader { geo in
+                TabView {
+                    ForEach (model.recipes) { r in
+                        if r.featured {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                VStack(spacing: 0) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(r.name)
+                                        .padding(5)
+                                }
+                            }
+                            .frame(width: geo.size.width-30, height: geo.size.height-100)
+                            .cornerRadius(10)
+                            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                            
+                        }
+                    }
+                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            VStack(alignment: .leading){
+                Text("Prep Time")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, vegetarian")
+            }
+        }.padding()
+        
     }
 }
 
